77e169eae9ebcf3f246f1a99f965164833ae94d6
Proizaac <762769@student.dusd.net>
2026-05-18 12:00:53 -0700

    5.2 #4

--- P4Unit5Trujillo/Assets/Script/Game  Manager.cs
+++ P4Unit5Trujillo/Assets/Script/Game  Manager.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using TMPro
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    
+    private int score;
+    public TextMeshProUGUI scoreText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() 
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+
     }
 
     // Update is called once per frame
@@ -25,6 +32,12 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            UpdateScore(5);
         }
+    }
+    private void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 }
